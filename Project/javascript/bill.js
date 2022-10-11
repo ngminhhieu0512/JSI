3e9c5f84e4ba723c0fe18121f4f27c504c13af41
ngminhhieu0512 <minhhieu5122004.git5@gmail.com>
2022-10-11 20:54:31 +0700

    Eleventh commit

--- Project/javascript/bill.js
+++ Project/javascript/bill.js
@@ -166,6 +166,6 @@ ${searchListRender}
 function deleleStorage() {
     alert("Thank you!");
     localStorage.clear();
-    window.location.href = "../html/index.html"
+    window.location.href = "../index.html"
 }
 
